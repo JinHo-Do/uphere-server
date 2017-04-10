94539278c5a600e87db2115e3cf284c5c4526229
JinHo Do <greatdjh@gmail.com>
2017-04-10 14:44:30 +0900

    add region setting

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if forever list | grep /var/nodejs/server/server.js > /dev/null
-then
-    echo "Server is running"
-else
-    echo "Server has been stopped and it will be started"
-    cd /var/nodejs/server
-    NODE_ENV=production forever start server.js
-fi
+# if forever list | grep /var/nodejs/server/server.js > /dev/null
+# then
+#     echo "Server is running"
+# else
+#     echo "Server has been stopped and it will be started"
+#     cd /var/nodejs/server
+#     NODE_ENV=production forever start server.js
+# fi
+
+forever stopall
+cd /var/nodejs/server
+NODE_ENV=production forever start server.js
